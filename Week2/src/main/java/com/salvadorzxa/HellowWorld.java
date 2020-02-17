99433ade7635e0930f51b52085f2942b341a4f40
Sal <SalvadorZX@Gmail.com>
2020-02-17 12:05:47 -0800

    Lots of new stuff

--- Week2/src/main/java/com/salvadorzxa/HellowWorld.java
+++ Week2/src/main/java/com/salvadorzxa/HellowWorld.java
@@ -1,7 +1,15 @@
 package com.salvadorzxa;
 
+import java.util.logging.Logger;
+
 public class HellowWorld {
+
+    private static Logger log = Logger.getLogger("HellowWorld");
+
     public static void main (String[] args){
-        System.out.println("Hello World Amdocs Dallas");
+
+    log.info("this is an info message");
+    System.out.println("Hello World Amdocs Dallas");
+
     }
 }
